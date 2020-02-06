fde9d3316071c372cbee07522f856c5dc91c458a
Kean Duque <vizen27@users.noreply.github.com>
2020-02-06 09:35:37 +0800

    Update kean.js
    oop let

--- kean.js
+++ kean.js
@@ -5,3 +5,14 @@ var msg = msgAlert("How are you Kean?");
 alert(msg);
 
 console.log(msg);
+
+let employee = {
+    id : 123,
+    name : "kean",
+    salary: 5000,
+    testFunction : function (){
+        return this.id + " " + this.name + " salary is : " + this.salary;
+    }
+};
+;
+console.log(employee.testFunction());
